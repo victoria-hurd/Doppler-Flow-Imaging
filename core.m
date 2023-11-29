73444376eecb0f04e4b1d5f5c8968a1258ca1e80
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2023-11-28 17:27:51 -0700

    Initializing core code script
    Added core script, added housekeeping, added data loading

---
+++ core.m
@@ -0,0 +1,17 @@
+% AUTHOR: Victoria Hurd
+% DATE CREATED: 11/28/2023
+% DATE LAST MODIFIED: 11/28/2023
+% PROJECT: MCEN 5127 Final Project
+% DESCRIPTION: Core code for MCEN 5127 Final Project. Eventually will be
+% run from wrapper.m as a function
+
+%% Housekeeping
+clear;clc;close all
+
+%% Data Read
+load("./data/flow_data.mat")
+
+%% Constants
+
+
+%% Calculations
