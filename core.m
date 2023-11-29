cb6a84ea8458c3aa427cfe4f00a18d030e13a43c
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2023-11-28 17:55:14 -0700

    Adding B-Mode Display
    Prelim attempt at adding B-Mode display capability. Issue with log compressing and adding dynamic range to displays since the data given within rf_bmode contains negatives, which can't be log compressed without creating complex vals.

--- core.m
+++ core.m
@@ -11,7 +11,33 @@ clear;clc;close all
 %% Data Read
 load("./data/flow_data.mat")
 
-%% Constants
+%% Display B-Mode Image
+% Display the log-compressed image using proper axes (with labels) and 
+% proportions with a 60 dB dynamic range, normalized to the brightest point
+% in the image. Provide a colorbar.
+figure
+hold on 
+% Issue here - need to log compress, but can't because rf_bmode has negs
+% This causes imaginaries once within log10()
+%h = surf(x,z,20*log10(rf_bmode/max(rf_bmode(:))));
+h = surf(x,z,rf_bmode/max(rf_bmode(:)));
+set(h,'LineStyle','none')
+title("Log-Compressed Phantom Image")
+xlabel("X Position [m]")
+ylabel("Z Position [m]")
+colormap(gray)
+colorbar
+ylim([min(z),max(z)])
+xlim([min(x),max(x)])
+set(gca, 'YDir','reverse')
+%clim([-60 0])
+hold off
 
+%% B-Mode Movie
 
-%% Calculations
+%% Baseband Demodulation
+
+%% Wall Filter
+
+%% Color Flow Doppler
+
