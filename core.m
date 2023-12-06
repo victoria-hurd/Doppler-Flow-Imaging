c7e787d54ae27e42f945ac8aa6ec315645d2686c
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2023-12-06 16:08:28 -0700

    Changing last edited date

--- core.m
+++ core.m
@@ -1,6 +1,6 @@
 % AUTHOR: Victoria Hurd
 % DATE CREATED: 11/28/2023
-% DATE LAST MODIFIED: 12/1/2023
+% DATE LAST MODIFIED: 12/6/2023
 % PROJECT: MCEN 5127 Final Project
 % DESCRIPTION: Core code for MCEN 5127 Final Project. Eventually will be
 % run from wrapper.m as a function
